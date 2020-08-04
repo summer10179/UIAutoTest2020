d4f3bdf57d05ed9ae1a0963846864bec084f833b
1939839514@qq.com <summer111111>
2020-08-04 17:49:31 +0800

    "tijiao "

--- src/main/java/com/shaxi/Dates/ConfigData.java
+++ src/main/java/com/shaxi/Dates/ConfigData.java
@@ -7,9 +7,7 @@ public class ConfigData {
        public  static String  host="http://172.19.188.179:8070/";
 //       设置静态常量数据
        public static String  host1="http://172.19.201.40:9080/shxjc-server/";
-       public static String Cookies="PSDSESSION=41d8def2-2e17-4522-9ead-d17f46a64980; EUNOMIASESSIONID=96e63ac7-e732-4a9c-8a3a-5e4bdbaadb15";
-       public static String Cookies1="JSESSIONID=21970B22B7C9E13025874515BCF5CCD8; PSDSESSION=b03bb0c7-3568-47b9-9dd8-5d329e6f4e42";
-@Test
+       public static String Cookies="PSDSESSION=e82b3984-6122-4d93-bf4b-111fb9871d20; EUNOMIASESSIONID=df4aa5e4-5460-4379-b311-cd757b82be37";
 public void gerCookie(){
        GetPSDSESSION getPSDSESSION1=new GetPSDSESSION();
        String str=getPSDSESSION1.getPSDSESSION();
